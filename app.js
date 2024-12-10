586187ebe0462238f667c8dd9c113345eec53e7d
Kayla Hunt <huntkay1@msu.edu>
2024-12-10 16:13:03 -0500

    Create signup form

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('node:path');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false}));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(3000, () => console.log('Listening on port 3000'));

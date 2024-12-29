6485243cd39cf5ba93882079129ed850412d8fa7
Kayla Hunt <huntkay1@msu.edu>
2024-12-29 18:17:52 -0500

    Add new message
    Users can add a new message to the database

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -8,7 +8,16 @@ async function getHomePageWithMessages(req, res) {
 }
 
 async function postNewMessage(req, res) {
-    console.log(req.body)
+    const message = req.body;
+    const username = req.user;
+    const currentDate = new Date().toLocaleString('en-US');;
+    
+    await pool.query('INSERT INTO messages ("text", "username", "add") VALUES ($1, $2, $3);', [
+        message,
+        username,
+        currentDate
+    ])
+    res.redirect('/home');
 }
 
 module.exports = {getHomePageWithMessages, postNewMessage}
